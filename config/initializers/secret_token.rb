3dfcc8dea7d138dda480c7b6db93a86bf219aa67
Ruby-Rails <ruby@zuplo.net>
2013-09-02 20:54:01 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mystore::Application.config.secret_token = '4d9a3426dc1b7e74d4f580f72b811fd05057d756e3494c7961cac646ec2f7dcccb86ba9467da9f10dfbada1bea742f4884a220fec0616932ff207d3cfa0a01b1'
